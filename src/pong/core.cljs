aa3c5830f60aad1238707ff56fde344828d52d40
tmtwd <t.williams.im@gmail.com>
2017-07-03 18:15:47 -0400

    ball and paddle move simultaneously

--- src/pong/core.cljs
+++ src/pong/core.cljs
@@ -6,9 +6,6 @@
 
 (def paddle-pos (atom [10 50]))
 
-;(aset ctx "fillStyle" "rgb(0, 0, 0)")
-;(. ctx fillRect 0, 0, (aget canvas "height"), (aget canvas "width"))
-
 (defn draw-background
   []
   (aset ctx "fillStyle" "rgb(0, 0, 0)")
@@ -29,15 +26,13 @@
     (Math/abs x)
     (- x)))
 
+(defn clear-and-draw-background
+  []
+  (clear-background)
+  (draw-background))
+
 (defn draw-ball
   []
-  (if (or (<= @x-coord 0) (>= @x-coord (aget canvas "width")))
-    (swap! direction update-in [0] negate))
-  (if (or (<= @y-coord 0) (>= @y-coord (aget canvas "height")))
-    (swap! direction update-in [1] negate))
-  (do
-    (swap! x-coord #(+ % (* 10 (first @direction))))
-    (swap! y-coord #(+ % (* 10 (second @direction)))))
   (. ctx beginPath)
   (. ctx arc @x-coord, @y-coord, 15, 0, (* 2 Math/PI))
   (aset ctx "fillStyle" "rgb(200, 0, 0)")
@@ -46,28 +41,40 @@
 
 (defn draw-paddle
   []
-  (clear-background)
-  (draw-background)
   (aset ctx "fillStyle" "rgb(200, 0, 0)")
   (. ctx fillRect (first @paddle-pos) (second @paddle-pos) 20 110))
+
+(defn draw-paddle-and-ball
+  []
+  (draw-paddle)
+  (draw-ball))
+
+(defn re-paint
+  []
+  (if (or (<= @x-coord 0) (>= @x-coord (aget canvas "width")))
+    (swap! direction update-in [0] negate))
+  (if (or (<= @y-coord 0) (>= @y-coord (aget canvas "height")))
+    (swap! direction update-in [1] negate))
+  (do
+    (swap! x-coord #(+ % (* 10 (first @direction))))
+    (swap! y-coord #(+ % (* 10 (second @direction)))))
+  (clear-and-draw-background)
+  (draw-paddle-and-ball)
+  (. js/window requestAnimationFrame re-paint))
 
 (defn init
   []
   (draw-background)
-  (draw-paddle)
-  #_(. js/window setInterval #(draw-ball) 50))
+  (draw-paddle-and-ball)
+  (. js/window requestAnimationFrame re-paint))
 
 (init)
-
-#_(draw-background)
-
 
 ;; code for players
 (defn move
       [e]
   (case (aget e "keyCode")
         38 (swap! paddle-pos update-in [1] #(- % 5))
-        40 (swap! paddle-pos update-in [1] #(+ % 5)))
-  (draw-paddle))
+        40 (swap! paddle-pos update-in [1] #(+ % 5))))
 
 (.addEventListener js/document "keydown" move false)
